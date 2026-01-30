d3abf7c52819d37f2c699d3753588758b1a3450e
fabioguida86 <fabio.guida86@gmail.com>
2026-01-30 19:29:40 +0100

    app.py

--- app.py
+++ app.py
@@ -3,12 +3,6 @@ import google.generativeai as genai
 import PIL.Image
 import json
 import os
-# --- CODICE DI VERIFICA AD-MAVEN ---
-st.markdown("""
-    <head>
-      <meta name='admaven-placement' content=Bqjw5rdgF>
-    </head>
-""", unsafe_allow_html=True)
 # --- 1. CONFIGURAZIONE CHIAVE E MODELLO ---
 try:
     MY_MASTER_KEY = st.secrets["GEMINI_KEY"]
@@ -195,6 +189,7 @@ with col2:
             st.session_state.congelati.pop(i); salva(); st.rerun()
 
 render_ad("STICKY_FOOTER_AD")
+
 
 
 
